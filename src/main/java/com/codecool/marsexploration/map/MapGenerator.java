d71e70f276df9103cad66a490361cc3a8f8b1f5f
claudiumihait <claudium.tudor@gmail.com>
2023-02-07 15:08:08 +0200

    Implement map generation and resource/shape placement

--- src/main/java/com/codecool/marsexploration/map/MapGenerator.java
+++ src/main/java/com/codecool/marsexploration/map/MapGenerator.java
@@ -1,6 +1,13 @@
 package com.codecool.marsexploration.map;
 
 import com.codecool.marsexploration.config.MapConfiguration;
+import com.codecool.marsexploration.data.Coordinate;
+import com.codecool.marsexploration.resource.Mineral;
+import com.codecool.marsexploration.resource.Water;
+import com.codecool.marsexploration.shape.Mountain;
+import com.codecool.marsexploration.shape.Pit;
+
+import java.util.List;
 
 public class MapGenerator {
 
@@ -11,9 +18,55 @@ public class MapGenerator {
     }
 
     public char[][] generate(){
+        char[][] map = new char[config.getWidth()][config.getWidth()];
         //generates a random map based on config and saves file
-        //TODO
+        if(config.isConfigValid()){
+            addResources(config.getWaters(), config.getMinerals(), map);
+
+            for(List<Mountain> mountainAreas : config.getMountainAreas())
+                setShapeOnMap(mountainAreas, null, map);
+
+            for (List<Pit> pitArea : config.getPitAreas())
+                setShapeOnMap(null, pitArea, map);
+            return map;
+        } else {
+            System.out.println("Provided config can not generate a valid map");
+            //TODO - handle not valid config
+        }
         return null;
      }
 
+    private void setShapeOnMap(List<Mountain> mountains, List<Pit> pits, char[][] map) {
+        if (mountains == null){
+            for (Pit pit : pits) {
+                for (Coordinate c : pit.getCoordinates()) {
+                    int x = c.x();
+                    int y = c.y();
+                    map[x][y] = pit.getSymbol();
+                }
+            }
+        } else {
+            for (Mountain mountain : mountains) {
+                for (Coordinate c : mountain.getCoordinates()) {
+                    int x = c.x();
+                    int y = c.y();
+                    map[x][y] = mountain.getSymbol();
+                }
+            }
+        }
+    }
+
+    private void addResources(List<Water> waters, List<Mineral> minerals, char[][] map) {
+        for(Mineral mineral : minerals){
+            int x = mineral.getLocation().x();
+            int y = mineral.getLocation().y();
+            map[x][y] = mineral.getSymbol();
+        }
+
+        for(Water water : waters){
+            int x = water.getLocation().x();
+            int y = water.getLocation().y();
+            map[x][y] = water.getSymbol();
+        }
+    }
 }
