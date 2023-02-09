d21d137c0d4a028d2f8f6f6d1b4e0195bbe8c71c
claudiumihait <claudium.tudor@gmail.com>
2023-02-09 18:17:27 +0200

    Implement psvm logic

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,46 +1,21 @@
 package com.codecool.marsexploration;
 
+import com.codecool.marsexploration.config.ConfigGenerator;
 import com.codecool.marsexploration.config.MapConfiguration;
 import com.codecool.marsexploration.map.MapGenerator;
-import com.codecool.marsexploration.resource.Mineral;
-import com.codecool.marsexploration.resource.Resource;
-import com.codecool.marsexploration.resource.ResourcePlacer;
-import com.codecool.marsexploration.resource.Water;
-import com.codecool.marsexploration.shape.*;
+import com.codecool.marsexploration.utils.Display;
 import com.codecool.marsexploration.utils.FileSaver;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import com.codecool.marsexploration.utils.Input;
 
 public class Application {
     public static void main(String[] args) {
-        Random random = new Random();
-
-
-        //COD DE PROBA
-
-        List<Shape> shapeList = new ArrayList<>();
-            shapeList.add(new MountainGenerator(30).generate());
-            shapeList.add(new MountainGenerator(25).generate());
-            shapeList.add(new MountainGenerator(20).generate());
-            shapeList.add(new PitGenerator(10).generate());
-            shapeList.add(new PitGenerator(15).generate());
-            shapeList.add(new PitGenerator(8).generate());
-        List<Resource> resourceList = new ArrayList<>();
-            resourceList.add(new Water());
-            resourceList.add(new Water());
-            resourceList.add(new Water());
-            resourceList.add(new Mineral());
-            resourceList.add(new Mineral());
-            resourceList.add(new Mineral());
-
-
-        MapConfiguration mapConfiguration = new MapConfiguration("My-Map",50,shapeList,resourceList);
-        MapGenerator mapGenerator = new MapGenerator(mapConfiguration, new FileSaver());
-        mapGenerator.generate();
-
-        //COD DE PROBA
-
+        Display display = new Display();
+        Input input = new Input(display);
+        FileSaver writer = new FileSaver();
+        ConfigGenerator configGenerator = new ConfigGenerator(input);
+        configGenerator.init();
+        MapConfiguration config = new MapConfiguration(configGenerator.getFileName(), configGenerator.getWidth(), configGenerator.getShapeList(),configGenerator.getResourceList());
+        MapGenerator generator = new MapGenerator(config, writer);
+        generator.generate();
     }
 }
